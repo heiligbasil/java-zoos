bfa884a8fd964cc190945912a12fbe14c78a994b
heiligbasil <basilief@yahoo.com>
2019-06-12 11:13:38 -0700

    Basil Havens - Java Zoos

---
+++ src/main/kotlin/com/lambdaschool/javazoos/JavaZoosApplication.kt
@@ -0,0 +1,12 @@
+package com.lambdaschool.javazoos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JavaZoosApplication
+
+fun main(args: Array<String>)
+{
+    runApplication<JavaZoosApplication>(*args)
+}
