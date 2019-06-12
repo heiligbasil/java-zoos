f3217214938f2f4fa8ed9298269e994afabb7255
heiligbasil <basilief@yahoo.com>
2019-06-12 15:41:28 -0700

    Added the controller

---
+++ src/main/kotlin/com/lambdaschool/javazoos/controllers/ZtaController.kt
@@ -0,0 +1,78 @@
+package com.lambdaschool.javazoos.controllers
+
+import com.lambdaschool.javazoos.models.Zoo
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder
+import java.net.URI
+import java.util.ArrayList
+import javax.validation.Valid
+
+@RestController
+@RequestMapping(value = ["/zta"])
+class ZtaController
+{
+    @Autowired
+    lateinit var ztaService: ZtaService
+
+    @GetMapping(value = ["/zoos"], produces = ["application/json"])
+    fun listAllZoos(): ResponseEntity<*>
+    {
+        val listOfZoos: MutableList<Zoo> = ztaService.findAll()
+        return ResponseEntity(listOfZoos, HttpStatus.OK)
+    }
+
+    @GetMapping(value = ["/zoo/{zooid}"], produces = ["application/json"])
+    fun listZooById(@PathVariable zooid: String): ResponseEntity<*>
+    {
+        val listOfOrds: Zoo = ztaService.findZooById(zooid)
+        return ResponseEntity(listOfOrds, HttpStatus.OK)
+    }
+
+    @GetMapping(value = ["/animals"], produces = ["application/json"])
+    fun countOfAnimalsInZoos(): ResponseEntity<*>
+    {
+        return ResponseEntity<ArrayList<CountOfAnimalsInZoos>>(ztaService.countStudentsInCourses, HttpStatus.OK)
+    }
+}
+
+@RestController
+@RequestMapping(value = ["/admin"])
+class AdminController
+{
+    @Autowired
+    lateinit var adminService: ZtaService
+
+    @DeleteMapping(value = ["/zoo/{zooid}"], consumes = ["application/json"])
+    fun deleteZooById(@PathVariable zooid: Long): ResponseEntity<*>
+    {
+        adminService!!.delete(zooid)
+
+        return ResponseEntity<Any>(HttpStatus.OK)
+    }
+
+    @PutMapping(value = ["/zoo/{zooid}"])
+    fun updateCustomerById(@RequestBody zooData: Zoo, @PathVariable zooid: Long): ResponseEntity<*>
+    {
+        adminService!!.update(zooData, zooid)
+
+        return ResponseEntity<Any>(HttpStatus.OK)
+    }
+
+    @PostMapping(value = ["/zoo"], consumes = ["application/json"], produces = ["application/json"])
+    fun addNewZoo(@Valid @RequestBody zooData: Zoo): ResponseEntity<*>
+    {
+        var zooCopy: Zoo = zooData
+        zooCopy = adminService!!.save(zooCopy)
+
+        val responseHeaders = HttpHeaders()
+        val newURI: URI = ServletUriComponentsBuilder.fromCurrentRequest().path("/{zooid}").buildAndExpand(zooCopy.zooid).toUri()
+
+        responseHeaders.location = newURI
+
+        return ResponseEntity<Any>(null, responseHeaders, HttpStatus.CREATED)
+    }
+}
